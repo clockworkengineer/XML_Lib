0189d2897164e00c4eddab667650c827a9b4f7f3
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2022-12-22 08:51:45 +0000

    Snapshot.c

--- classes/implementation/platform/windows/Converter.cpp
+++ classes/implementation/platform/windows/Converter.cpp
@@ -69,23 +69,6 @@ std::string Converter::toUtf8(const std::u16string &utf16) const
 /// <summary>
 /// Convert utf8 <-> utf32 strings.
 /// </summary>
-// std::u32string Converter::toUtf32(const std::string &utf8) const
-// {
-//   std::wstring wideString(BytesToWideChar(utf8.c_str(), static_cast<int>(utf8.length())), 0);
-//   BytesToWideChar(utf8.c_str(), static_cast<int>(utf8.length()), &wideString[0],
-//   static_cast<int>(wideString.length())); return (std::u32string{ wideString.begin(), wideString.end() });
-// }
-// std::string Converter::toUtf8(const std::u32string &utf32) const
-// {
-//   std::wstring wideString{ utf32.begin(), utf32.end() };
-//   std::string bytes(WideCharToBytes(&wideString[0], static_cast<int>(wideString.length())), 0);
-//   WideCharToBytes(&wideString[0], -1, &bytes[0], static_cast<int>(bytes.length()));
-//   return bytes;
-// }
-//std::string Converter::toUtf8(char32_t utf32) const { return (m_UTF32.to_bytes(utf32)); }
-/// <summary>
-/// Convert utf8 <-> utf32 strings.
-/// </summary>
 std::u32string Converter::toUtf32(const std::string &utf8) const { return (m_UTF32.from_bytes(utf8)); }
 std::string Converter::toUtf8(const std::u32string &utf32) const { return (m_UTF32.to_bytes(utf32)); }
 std::string Converter::toUtf8(char32_t utf32) const { return (m_UTF32.to_bytes(utf32)); }
