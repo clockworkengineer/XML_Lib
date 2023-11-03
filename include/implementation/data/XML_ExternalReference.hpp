8baad83847da7fd445baf47d1c6eaf7934d79ffc
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-11-03 19:49:49 +0000

    CLean code pass.

--- include/implementation/data/XML_ExternalReference.hpp
+++ include/implementation/data/XML_ExternalReference.hpp
@@ -8,7 +8,7 @@ struct XMLExternalReference
 {
   // Constructors/Destructors
   XMLExternalReference(const std::string &type, const std::string &systemID = "", const std::string publicID = "")
-    : m_type(type), m_systemID(systemID), m_publicID(publicID)
+    : type(type), systemID(systemID), publicID(publicID)
   {}
   XMLExternalReference(const XMLExternalReference &other) = default;
   XMLExternalReference &operator=(const XMLExternalReference &other) = default;
@@ -16,14 +16,14 @@ struct XMLExternalReference
   XMLExternalReference &operator=(XMLExternalReference &&other) = default;
   ~XMLExternalReference() = default;
   // Get external reference details
-  const std::string &getType() const { return (m_type); }
-  const std::string &getSystemID() const { return (m_systemID); }
-  const std::string &getPublicID() const { return (m_publicID); }
+  const std::string &getType() const { return (type); }
+  const std::string &getSystemID() const { return (systemID); }
+  const std::string &getPublicID() const { return (publicID); }
 
 private:
   // Reference type and ids
-  std::string m_type{};
-  std::string m_systemID{};
-  std::string m_publicID{};
+  std::string type{};
+  std::string systemID{};
+  std::string publicID{};
 };
 }// namespace XML_Lib
