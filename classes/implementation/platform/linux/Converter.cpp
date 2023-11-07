1c5fb6f26f94cfc99010793f3a1ca6986d3e4a36
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2023-11-07 17:17:13 +0000

    Clean code pass.

--- classes/implementation/platform/linux/Converter.cpp
+++ classes/implementation/platform/linux/Converter.cpp
@@ -19,19 +19,19 @@ static std::wstring_convert<std::codecvt_utf8_utf16<char32_t>, char32_t> utf32Co
 /// <summary>
 /// Convert to UTF-8 strings.
 /// </summary>
-std::string Converter::toUtf8(char16_t utf16) const { return (toUtf8(std::u16string(utf16, 1))); }
-std::string Converter::toUtf8(const std::u16string &utf16) const { return (utf16Convert.to_bytes(utf16)); }
-std::string Converter::toUtf8(char32_t utf32) const { return (utf32Convert.to_bytes(utf32)); }
-std::string Converter::toUtf8(const std::u32string &utf32) const { return (utf32Convert.to_bytes(utf32)); }
+std::string IConverter::toUtf8(char16_t utf16) const { return (toUtf8(std::u16string(utf16, 1))); }
+std::string IConverter::toUtf8(const std::u16string &utf16) const { return (utf16Convert.to_bytes(utf16)); }
+std::string IConverter::toUtf8(char32_t utf32) const { return (utf32Convert.to_bytes(utf32)); }
+std::string IConverter::toUtf8(const std::u32string &utf32) const { return (utf32Convert.to_bytes(utf32)); }
 
 /// <summary>
 /// Convert to UTF-16 strings.
 /// </summary>
-std::u16string Converter::toUtf16(const std::string &utf8) const { return (utf16Convert.from_bytes(utf8)); }
+std::u16string IConverter::toUtf16(const std::string &utf8) const { return (utf16Convert.from_bytes(utf8)); }
 
 /// <summary>
 /// Convert to UTF-32 strings.
 /// </summary>
-std::u32string Converter::toUtf32(const std::string &utf8) const { return (utf32Convert.from_bytes(utf8)); }
-std::u32string Converter::toUtf32(const std::u16string &utf16) const { return (toUtf32(toUtf8(utf16))); }
+std::u32string IConverter::toUtf32(const std::string &utf8) const { return (utf32Convert.from_bytes(utf8)); }
+std::u32string IConverter::toUtf32(const std::u16string &utf16) const { return (toUtf32(toUtf8(utf16))); }
 }// namespace XML_Lib
