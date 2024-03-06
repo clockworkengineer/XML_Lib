6b6cbf263eb33f997dc02313135f95089465b8aa
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-03-06 16:57:01 +0000

    Add isType functions.

--- include/implementation/variants/XML_Variant.hpp
+++ include/implementation/variants/XML_Variant.hpp
@@ -17,7 +17,17 @@ struct Variant
   virtual ~Variant() = default;
   // Get/Set XNode type
   [[nodiscard]] Type getType() const { return (xmlNodeType); }
-
+  [[nodiscard]] bool isProlog() { return (xmlNodeType == Type::prolog); }
+  [[nodiscard]] bool isDeclaration() { return (xmlNodeType == Type::declaration); }
+  [[nodiscard]] bool isRoot() { return (xmlNodeType == Type::root); }
+  [[nodiscard]] bool isSelf() { return (xmlNodeType == Type::self); }
+  [[nodiscard]] bool isElement() { return (xmlNodeType == Type::element); }
+  [[nodiscard]] bool isContent() { return (xmlNodeType == Type::content); }
+  [[nodiscard]] bool isEntity() { return (xmlNodeType == Type::entity); }
+  [[nodiscard]] bool isComment() { return (xmlNodeType == Type::comment); }
+  [[nodiscard]] bool isCDATA() { return (xmlNodeType == Type::cdata); }
+  [[nodiscard]] bool isPI() { return (xmlNodeType == Type::pi); }
+  [[nodiscard]] bool isDTD() { return (xmlNodeType == Type::dtd); }
   // Get XNode children reference
   std::vector<XNode> &getChildren() { return (children); }
   const std::vector<XNode> &getChildren() const { return (children); }
