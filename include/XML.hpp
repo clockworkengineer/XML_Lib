71cfa5fcb04233cdc87149b2c3d01de6dadb6b05
Rob Tizzard <clockworkengineer@users.noreply.github.com>
2024-04-15 18:29:04 +0100

    SNapshot.

--- include/XML.hpp
+++ include/XML.hpp
@@ -65,12 +65,12 @@ public:
   // Stringify XML object to destination text
   void stringify(IDestination &destination);
   void stringify(IDestination &&destination);
-  // Read/Write XML from file
+  // Read/Write XML to/from file
   static const std::string fromFile(const std::string &fileName);
   static void toFile(const std::string &fileName, const std::string &xmlString, Format format = Format::utf8);
-  // Get JSON file format
+  // Get XML file format
   static Format getFileFormat(const std::string &fileName);
-  
+
 private:
   const std::unique_ptr<XML_Impl> implementation;
 };
