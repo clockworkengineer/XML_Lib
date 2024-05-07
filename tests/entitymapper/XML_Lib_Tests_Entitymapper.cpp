37e57a5c4d683ec119f73cf2dd5407c03e6f65f7
clockworkengineer <robert_tizzard@hotmail.com>
2024-05-07 18:28:52 +0100

    Add more tests.

---
+++ tests/entitymapper/XML_Lib_Tests_Entitymapper.cpp
@@ -0,0 +1,47 @@
+//
+// Unit Tests: XML_Lib_Tests_Entitymapper
+//
+// Description: XML Entity Mapper unit tests.
+//
+
+#include "XML_Lib_Tests.hpp"
+
+using namespace XML_Lib;
+
+TEST_CASE("XML entity mapper useage tests cases.", "[XML][EntityMapper]")
+{
+  SECTION("Create entity mapper does not throw execption..", "[XML][PaEntityMapperrse][Create]")
+  {
+    REQUIRE_NOTHROW(XML_EntityMapper());
+  }
+  SECTION("Create entity mapper intialised default with &amp; entry.", "[XML][EntityMapper][DefaultValues]")
+  {
+    XML_EntityMapper entityMapper;
+    REQUIRE_FALSE(!entityMapper.isPresent("&amp;"));
+    REQUIRE(entityMapper.getInternal("&amp;")=="&#x26;");
+  }
+  SECTION("Create entity mapper intialised default with &quot; entry.", "[XML][EntityMapper][DefaultValues]")
+  {
+    XML_EntityMapper entityMapper;
+    REQUIRE_FALSE(!entityMapper.isPresent("&quot;"));
+    REQUIRE(entityMapper.getInternal("&quot;")=="&#x22;");
+  }
+  SECTION("Create entity mapper intialised default with &apos; entry.", "[XML][EntityMapper][DefaultValues]")
+  {
+    XML_EntityMapper entityMapper;
+    REQUIRE_FALSE(!entityMapper.isPresent("&apos;"));
+    REQUIRE(entityMapper.getInternal("&apos;")=="&#x27;");
+  }
+  SECTION("Create entity mapper intialised default with &lt; entry.", "[XML][EntityMapper][DefaultValues]")
+  {
+    XML_EntityMapper entityMapper;
+    REQUIRE_FALSE(!entityMapper.isPresent("&lt;"));
+    REQUIRE(entityMapper.getInternal("&lt;")=="&#x3C;");
+  }
+  SECTION("Create entity mapper intialised default with &gt; entry.", "[XML][EntityMapper][DefaultValues]")
+  {
+    XML_EntityMapper entityMapper;
+    REQUIRE_FALSE(!entityMapper.isPresent("&gt;"));
+    REQUIRE(entityMapper.getInternal("&gt;")=="&#x3E;");
+  }
+}
