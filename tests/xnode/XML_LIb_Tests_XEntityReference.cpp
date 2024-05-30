4e2ca59e686a45eb5c009f96733f9b74d3192bf1
clockworkengineer <robert_tizzard@hotmail.com>
2024-05-30 20:52:45 +0100

    Add more tests.

--- tests/xnode/XML_LIb_Tests_XEntityReference.cpp
+++ tests/xnode/XML_LIb_Tests_XEntityReference.cpp
@@ -10,21 +10,26 @@
 
 using namespace XML_Lib;
 
+XMLValue createXMLValue()
+{
+  return(XMLValue("unparsed", "parsed"));
+}
+
 TEST_CASE("Create and use XEntityReference XNode.","[XNode][XEntityReference][API]")
 {
   SECTION("Create XEntityReference XNode.", "[XML][XNode][XEntityReference][API]")
   {
-    REQUIRE_NOTHROW(XEntityReference(XMLValue("unparsed", "parsed")));
+    REQUIRE_NOTHROW(XEntityReference(createXMLValue()));
   }
   SECTION("Create XEntityReference XNode and fetch value related to it.", "[XML][XNode][XEntityReference][API]")
   {
-    XEntityReference xEntityReference = XEntityReference(XMLValue("unparsed", "parsed"));
+    XEntityReference xEntityReference = XEntityReference(createXMLValue());
     REQUIRE(xEntityReference.value().getParsed() == "parsed");
     REQUIRE(xEntityReference.value().getUnparsed() == "unparsed");
   }
   SECTION("Create and use XEntityReference using make/XRef API.", "[XML][XNode][XEntityReference][Make][XRef][API]")
   {
-    XNode xNode = XNode::make<XEntityReference>(XMLValue("unparsed", "parsed"));
+    XNode xNode = XNode::make<XEntityReference>(createXMLValue());
     REQUIRE_FALSE(!xNode.isEntity());
     REQUIRE(XRef<XEntityReference>(xNode).value().getParsed() == "parsed");
     REQUIRE(XRef<XEntityReference>(xNode).value().getUnparsed() == "unparsed");
---
+++ tests/xnode/XML_Lib_Tests_XDTD.cpp
@@ -0,0 +1,36 @@
+//
+// Unit Tests: XML_Lib_Tests_XDTD.cpp
+//
+// Description: Create and use XDTD XNode
+//
+
+#include "XML_Lib_Tests.hpp"
+
+using namespace XML_Lib;
+
+TEST_CASE("Create and use XDTD XNode.","[XNode][XDTD][API]")
+{
+  SECTION("Create XDTD XNode.", "[XML][XNode][XDTD][API]")
+  {
+    XML_EntityMapper mapper;
+    REQUIRE_NOTHROW(XDTD(mapper));
+  }
+  SECTION("Create XDTD XNode and check root name hasnt been ser.", "[XML][XNode][XDTD][API]")
+  {
+    XML_EntityMapper mapper;
+    XDTD xDTD (mapper);
+    REQUIRE(xDTD.getRootName()=="");
+    REQUIRE(xDTD.getElementCount()==0);
+  }
+  SECTION("Create and use XDTD using make/XRef API.", "[XML][XNode][XDTD][Make][XRef][API]")
+  {
+    XML_EntityMapper mapper;
+    XNode xNode = XNode::make<XDTD>(mapper);
+    REQUIRE_FALSE(!xNode.isDTD());
+    REQUIRE(XRef<XDTD>(xNode).getRootName()=="");
+    REQUIRE(XRef<XDTD>(xNode).getElementCount()==0);
+  }
+}
+
+
+
