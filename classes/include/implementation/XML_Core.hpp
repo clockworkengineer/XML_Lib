f61eca472d3518b9ef401af03265b4e09822d75e
clockworkengineer <robert_tizzard@hotmail.com>
2024-06-21 16:04:08 +0100

    Include file refactor.

--- classes/include/implementation/XML_Core.hpp
+++ classes/include/implementation/XML_Core.hpp
@@ -25,17 +25,21 @@
 #include "XML_XComment.hpp"
 #include "XML_XPI.hpp"
 #include "XML_XDTD.hpp"
+
 // XNode
 #include "XML_XNode.hpp"
 #include "XML_Content.hpp"
 #include "XML_Reference.hpp"
 #include "XML_Index.hpp"
 #include "XML_Creation.hpp"
+
 // Entity Mapper
 #include "XML_EntityMapper.hpp"
+
 // IO
 #include "XML_Sources.hpp"
 #include "XML_Destinations.hpp"
+
 // Common
 #include "XML_Error.hpp"
 #include "XML_Character.hpp"
