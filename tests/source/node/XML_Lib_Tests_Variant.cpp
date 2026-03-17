27dc7b457f51365537052707665b65bbb6e017f2
clockworkengineer <robert_tizzard@hotmail.com>
2026-03-17 18:06:29 +0000

    add tests

--- tests/source/node/XML_Lib_Tests_Variant.cpp
+++ tests/source/node/XML_Lib_Tests_Variant.cpp
@@ -32,3 +32,46 @@ TEST_CASE("Create and use Variant", "[XML][Node][Variant][API]")
     REQUIRE(variant.getContents() == "");
   }
 }
+// Additional tests for Variant
+TEST_CASE("Variant edge cases and integration", "[XML][Node][Variant][Edge][Integration]")
+{
+  SECTION("Variant copy/move semantics", "[XML][Variant][CopyMove]")
+  {
+    Variant original(Variant::Type::base);
+    Variant moved(std::move(original));
+    REQUIRE(moved.getNodeType() == Variant::Type::base);
+  }
+
+  SECTION("Variant with different node types", "[XML][Variant][NodeType]")
+  {
+    Variant prolog(Variant::Type::prolog);
+    Variant element(Variant::Type::element);
+    Variant content(Variant::Type::content);
+    REQUIRE(prolog.getNodeType() == Variant::Type::prolog);
+    REQUIRE(element.getNodeType() == Variant::Type::element);
+    REQUIRE(content.getNodeType() == Variant::Type::content);
+    REQUIRE(element.isNameable());
+    REQUIRE(element.isIndexable());
+    REQUIRE_FALSE(prolog.isNameable());
+    REQUIRE(prolog.isIndexable());
+  }
+
+  SECTION("Variant integration with derived types", "[XML][Variant][Integration]")
+  {
+    Content contentVariant("integration test");
+    REQUIRE(contentVariant.getNodeType() == Variant::Type::content);
+    REQUIRE(contentVariant.getContents() == "integration test");
+    contentVariant.addContent("123");
+    REQUIRE(contentVariant.getContents() == "integration test123");
+  }
+
+  SECTION("Variant addChild edge case", "[XML][Variant][AddChild]")
+  {
+    Variant variant;
+    Node child;
+    variant.addChild(child);
+    REQUIRE(variant.getChildren().size() == 1);
+    variant.addChild(Node());
+    REQUIRE(variant.getChildren().size() == 2);
+  }
+}
