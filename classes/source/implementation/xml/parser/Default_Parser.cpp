c69e8e0a1df2e48e2239e6d3d1145b5943d5b05e
clockworkengineer <robert_tizzard@hotmail.com>
2026-03-20 16:02:41 +0000

    save changes

--- classes/source/implementation/xml/parser/Default_Parser.cpp
+++ classes/source/implementation/xml/parser/Default_Parser.cpp
@@ -324,8 +324,13 @@ Node Default_Parser::parseDeclaration(ISource &source)
     } else {
       throw SyntaxError(source.getPosition(), "Version missing from declaration.");
     }
-    if (source.match("encoding")) { encoding = parseDeclarationAttribute(source, "encoding", { "UTF-8", "UTF-16" }); }
-    if (source.match("standalone")) { standalone = parseDeclarationAttribute(source, "standalone", { "yes", "no" }); }
+     if (source.match("encoding")) {
+      // XML 1.0 allows a wide range of encodings, not just UTF-8 and UTF-16
+      static const std::set<std::string> allowedEncodings = {
+        "UTF-8", "UTF-16", "ISO-10646-UCS-2", "ISO-10646-UCS-4", "ISO-8859-1", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5", "ISO-8859-6", "ISO-8859-7", "ISO-8859-8", "ISO-8859-9", "ISO-2022-JP", "ISO-2022-JP-2", "ISO-2022-KR", "ISO-2022-CN", "EUC-JP", "EUC-KR", "EUC-CN", "SHIFT_JIS", "US-ASCII"
+      };
+      encoding = parseDeclarationAttribute(source, "encoding", allowedEncodings);
+    }   if (source.match("standalone")) { standalone = parseDeclarationAttribute(source, "standalone", { "yes", "no" }); }
     if (source.match("encoding")) {
       throw SyntaxError(source.getPosition(), "Incorrect order for version, encoding and standalone attributes.");
     }
